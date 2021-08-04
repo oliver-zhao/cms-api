a08e066ee9ceacb1665fb9821694cf8e6841c5c6
Oliver Zhao <Oliver.Zhao@team.telstra.com>
2021-08-04 12:37:59 +1000

    demo the request body and response

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,37 @@ const  fs = require('fs');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  const firstName = req.params.firstName;
+  console.log("firstName="+firstName);
   let userFile = fs.readFileSync('db/users.json');
   let users = JSON.parse(userFile);
   res.json(users);
+});
+
+router.post('/', function(req, res, next) {
+   /* TODO 
+     1. get user information from the request body.
+     2. add user json data to db/users.json 
+     ( 
+       how to read and write the file
+       how to covert the json object to string , and string to object
+      )
+   */
+   const firstName = req.body.lastName;
+   const lastName = req.body.firstName;
+   console.log(req.params);
+  // const email = req.body.email;
+  // const password = req.body.password;
+  console.log(firstName);
+  console.log(lastName);
+  // console.log(email);
+  res.json({message:"Create user successfully"});
+});
+
+router.delete('/:email', function(req, res, next){
+    const email = req.params.email;
+    console.log(email);
+    res.status(400).send({message:"Delete user failed"});
 });
 
 module.exports = router;
