1fce5df90fa46cbd821a7c4e938e74da0e0520e2
spartacus13 <john.wong.s.m@gmail.com>
2021-08-11 15:00:11 +1000

    testing

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const  fs = require('fs');
 
+router.use(express.json());
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  const firstName = req.params.firstName;
-  console.log("firstName="+firstName);
+  //const firstName = req.params.firstName;
+  //console.log("firstName="+firstName);
   let userFile = fs.readFileSync('db/users.json');
   let users = JSON.parse(userFile);
   res.json(users);
@@ -17,18 +19,17 @@ router.post('/', function(req, res, next) {
      2. add user json data to db/users.json 
      ( 
        how to read and write the file
-       how to covert the json object to string , and string to object
+       how to convert the json object to string , and string to object
       )
    */
-   const firstName = req.body.lastName;
-   const lastName = req.body.firstName;
-   console.log(req.params);
-  // const email = req.body.email;
-  // const password = req.body.password;
-  console.log(firstName);
-  console.log(lastName);
-  // console.log(email);
-  res.json({message:"Create user successfully"});
+  //const firstName = req.query.firstName;
+  //const lastName = req.query.lastName;
+  //const email = req.body.email;
+  //const password = req.body.password;
+  console.log(req.body.firstName);
+  console.log(req.body.lastName);
+  //console.log(email);
+  res.xml({message:"Create user successfully"});
 });
 
 router.delete('/:email', function(req, res, next){
