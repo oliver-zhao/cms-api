9e6393b72f2eb97ec50947bba764759abc9e19ad
Oliver Zhao <zhaoyaoqing@gmail.com>
2021-08-16 17:07:00 +1000

    change version to 1.0.2

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/version', function(req, res, next) {
-  res.json({'version' : 'v1.0.1'});
+  res.json({'version' : 'v1.0.2'});
 });
 
 module.exports = router;
